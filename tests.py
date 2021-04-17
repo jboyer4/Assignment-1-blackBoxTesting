b9d8f68e46ccddf48269ddcf33495e5b9baa477e
Justin Boyer <justinbboyer@gmail.com>
2021-04-17 10:48:19 -0700

    Add visa tests

--- tests.py
+++ tests.py
@@ -1,9 +1,32 @@
 import unittest
 from credit_card_validator import credit_card_validator
 
+
 class TestCC(unittest.TestCase):
+    # Should fail with no number
+    # edge case
     def test1(self):
-        self.assertFalse(False)
+        self.assertFalse(credit_card_validator(''))
+
+    # Should validate a good visa
+    # Category Partition Testing
+    def test2(self):
+        self.assertTrue(credit_card_validator('4156154215421545'))
+
+    # Should fail Luhn valid visa with 15 digits
+    # Picked using Category Partition Testing
+    def test3(self):
+        self.assertFalse(credit_card_validator('415615421542154'))
+
+    # Should fail Luhn valid visa with 17 digits
+    # Picked using Category Partition Testing
+    def test4(self):
+        self.assertFalse(credit_card_validator('48961021637942063'))
+
+    # Should fail Luhn invalid with 16 digits
+    # Picked using Category Partition Testing
+    def test5(self):
+        self.assertFalse(credit_card_validator('4896102163794206'))
 
 
 if __name__ == '__main__':
